d710badd0b9729b9fc04f6d2b6014071b4578bf7
s1n7ax <srineshnisala@gmail.com>
2023-07-25 11:35:54 +0530

    feat(dev): testcontainers to run postgres for testing

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,18 @@ mod tag;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // sqlx::migrate!("db/migrations")
-    //     .run(&pool)
-    //     .await
-    //     .expect("Database migration failed");
+    let app_host = env::var("APP_HOST").expect("env.APP_HOST not found");
+    let app_port = env::var("APP_PORT").expect("env.APP_PORT not found");
 
-    let host = env::var("HOST").expect("env.HOST not found");
-
-    let addr = host
+    let addr = format!("{}:{}", app_host, app_port)
         .parse::<SocketAddr>()
-        .unwrap_or(SocketAddr::from(([0, 0, 0, 0], 3000)));
+        .expect(&format!(
+            "Faild to create a socket addr from {}:{}",
+            app_host, app_port
+        ));
 
-    let routes = routes().await;
+    let app_state = get_state().await;
+    let routes = get_routes(app_state);
 
     axum::Server::bind(&addr)
         .serve(routes.into_make_service())
@@ -33,23 +33,34 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-async fn db_pool() -> Pool<Postgres> {
-    let db_url = env::var("DATABASE_URL").expect("env.DATABASE_URL not found");
+async fn migrate_db(pool: &Pool<Postgres>) {
+    sqlx::migrate!("db/migrations")
+        .run(pool)
+        .await
+        .expect("Database migration failed");
+}
+
+async fn get_db_pool() -> Pool<Postgres> {
+    let db_host = env::var("DB_HOST").expect("env.DB_USER not found");
+    let db_port = env::var("DB_PORT").expect("env.DB_PORT not found");
+    let db_name = env::var("DB_NAME").expect("env.DB_NAME not found");
+    let db_user = env::var("DB_USER").expect("env.DB_USER not found");
+    let db_pass = env::var("DB_PASSWORD").expect("env.DB_PASSWORD not found");
+
+    let db_url =
+        format!("postgres://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}");
 
     sqlx::postgres::PgPool::connect(&db_url)
         .await
         .expect("Unable to connect to the database")
 }
 
-async fn state() -> AppState {
-    let pool = db_pool().await;
+async fn get_state() -> AppState {
+    let pool = get_db_pool().await;
     AppState { pool }
 }
 
-#[allow(dead_code)]
-async fn routes() -> Router {
-    let app_state = state().await;
-
+fn get_routes(app_state: AppState) -> Router {
     Router::new()
         .route(
             "/",
@@ -64,71 +75,62 @@ async fn routes() -> Router {
 #[cfg(test)]
 mod test {
     use hyper::{self, Body, Request};
-    use std::{collections::HashMap, net::TcpListener};
-    use testcontainers::{core::WaitFor, Image};
+    use std::{net::TcpListener, time::Duration};
+    use testcontainers::{
+        clients::Cli, core::WaitFor, images::generic::GenericImage, Container,
+    };
 
     use super::*;
 
-    pub async fn start_service() {
+    pub async fn start<'a>(docker: &'a Cli) {
+        let container = start_postgres(docker);
+        let db_port = container.get_host_port_ipv4(5432);
+        let conn_str =
+            format!("postgres://root:root@0.0.0.0:{}/notepad", db_port);
+
+        let pool = sqlx::postgres::PgPool::connect(&conn_str)
+            .await
+            .expect("Unable to connect to the database");
+
+        start_service(AppState { pool }).await;
+    }
+
+    pub async fn start_service(app_state: AppState) {
         let listener = TcpListener::bind("0.0.0.0:3000").unwrap();
 
         tokio::spawn(async move {
             axum::Server::from_tcp(listener)
                 .unwrap()
-                .serve(routes().await.into_make_service())
+                .serve(get_routes(app_state).into_make_service())
                 .await
                 .unwrap();
         });
     }
 
-    pub struct Postgres {
-        env_vars: HashMap<String, String>,
-    }
-
-    impl Default for Postgres {
-        fn default() -> Self {
-            return Self {
-                env_vars: HashMap::from([(
-                    String::from("POSTGRES_PASSWORD"),
-                    String::from("root"),
-                )]),
-            };
-        }
-    }
-
-    impl Image for Postgres {
-        type Args = ();
-
-        fn name(&self) -> String {
-            String::from("postgres")
-        }
-
-        fn tag(&self) -> String {
-            String::from("latest")
-        }
-
-        fn ready_conditions(&self) -> Vec<testcontainers::core::WaitFor> {
-            vec![WaitFor::message_on_stderr(
+    pub fn start_postgres<'a>(docker: &'a Cli) -> Container<'a, GenericImage> {
+        let image = GenericImage::new("postgres", "latest")
+            .with_env_var("POSTGRES_PASSWORD", "root")
+            .with_env_var("POSTGRES_USER", "root")
+            .with_env_var("POSTGRES_DB", "notepad")
+            .with_wait_for(WaitFor::message_on_stdout(
                 "database system is ready to accept connections",
-            )]
-        }
+            ))
+            // for some reason eventhough container prints "database is ready"
+            // connection fails and adding 1 sec fixes it atlease on this pc
+            .with_wait_for(WaitFor::Duration {
+                length: Duration::from_secs(1),
+            });
 
-        fn env_vars(
-            &self,
-        ) -> Box<dyn Iterator<Item = (&String, &String)> + '_> {
-            Box::new(self.env_vars.iter())
-        }
+        docker.run(image)
     }
 
     #[tokio::test]
     async fn service_is_running() {
-        start_service().await;
-
-        let docker = testcontainers::clients::Cli::docker();
-        docker.run(Postgres::default());
+        // start all the services
+        let docker: Cli = testcontainers::clients::Cli::docker();
+        start(&docker).await;
 
         let client = hyper::client::Client::new();
-
         let response = client
             .request(
                 Request::builder()
